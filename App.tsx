e7a742650adca62b66297e3f716ba3488c26c36b
Ademar de Souza Novaes Junior <ademar.junior@havan.com.br>
2025-04-28 07:52:14 -0300

    realizado prebuild

--- App.tsx
+++ App.tsx
@@ -10,7 +10,8 @@ import { CartContextProvider } from './src/contexts/CartContext';
 import { ThemeProvider } from '@react-navigation/native';
 import { OneSignal } from 'react-native-onesignal'
 
-OneSignal.initialize("bla-bla-blaID")
+OneSignal.initialize("04e2144f-5359-4da2-9902-32d98505304c")
+OneSignal.Notifications.requestPermission(true)
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
